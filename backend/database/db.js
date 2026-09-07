6e897ad5c60a9f6745cb79e0056973ddfeb8113e
Yash Sathe <yashsathe1357@gmail.com>
2026-09-07 22:25:36 +0530

    add database connection setup in server and create db.js for MongoDB connection

---
+++ backend/database/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/note-app`);
+    console.log("MongoDB connected Successfully");
+  } catch (error) {
+    console.log("MongoDB connection error ", error);
+  }
+};
+export default connectDB;
