e2bc20b3bc973f9a6aa5b95b38bee78ab557cd86
Yash Sathe <yashsathe1357@gmail.com>
2026-09-16 22:05:50 +0530

    feat: add user validation middleware with yup schema for registration

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -9,15 +9,16 @@ import {
   verifyOTP,
 } from "../controller/user.controller.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.middleware.js";
+import { userSchema, validateUser } from "../validators/user.validate.js";
 
 const router = express.Router();
 
-router.post("/register", userRegister);
+router.post("/register", validateUser(userSchema), userRegister);
 router.post("/verify", verification);
 router.post("/login", userLogin);
 router.post("/logout", isAuthenticated, userLogout);
 router.post("/forgot-password", forgotPassword);
 router.post("/verify-otp/:email", verifyOTP);
-router.post("/change-password/:email",changePassword);
+router.post("/change-password/:email", changePassword);
 
 export default router;
